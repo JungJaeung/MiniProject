253ce510b4a70a997f5a78a0bd01fc1b0312c1e8
JungJaeung <hello.git.jk3576350@naver.com>
2022-11-23 23:58:01 +0900

    ERD 11/23일에 수정한 파일 추

--- planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/controller/BoardController.java
+++ planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/controller/BoardController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ezen.springplanetrip.service.account.BoardService;
+import com.ezen.springplanetrip.service.Board.BoardService;
 import com.ezen.springplanetrip.vo.BoardVO;
 
 @Controller
--- planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/service/Board/impl/BoardServiceImpl.java
+++ planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/service/Board/impl/BoardServiceImpl.java
@@ -1,11 +1,11 @@
-package com.ezen.springplanetrip.service.account.impl;
+package com.ezen.springplanetrip.service.Board.impl;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ezen.springplanetrip.service.account.BoardService;
+import com.ezen.springplanetrip.service.Board.BoardService;
 import com.ezen.springplanetrip.vo.BoardVO;
 
 @Service
