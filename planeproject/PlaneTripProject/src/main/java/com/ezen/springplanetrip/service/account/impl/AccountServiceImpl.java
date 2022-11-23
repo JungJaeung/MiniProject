8c48f1726952c642c881b2bc9afb2fd8c766ad57
JungJaeung <hello.git.jk3576350@naver.com>
2022-11-23 14:59:59 +0900

    Controller 정보 클래스 다이어그램 수정

--- planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/service/account/impl/AccountServiceImpl.java
+++ planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/service/account/impl/AccountServiceImpl.java
@@ -3,8 +3,10 @@ package com.ezen.springplanetrip.service.account.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ezen.springplanetrip.service.account.AccountService;
+
 @Service
-public class AccountServiceImpl {
+public class AccountServiceImpl implements AccountService{
 	@Autowired
 	private AccountDAO accountDAO;
 }
