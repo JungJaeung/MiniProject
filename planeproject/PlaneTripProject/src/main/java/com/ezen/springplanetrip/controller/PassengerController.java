96bff41207d4dd1acbcf017f726c8925f9050bc3
JungJaeung <hello.git.jk3576350@naver.com>
2022-12-01 10:44:14 +0900

    승객 정보 받기 수정함

--- planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/controller/PassengerController.java
+++ planeproject/PlaneTripProject/src/main/java/com/ezen/springplanetrip/controller/PassengerController.java
@@ -1,13 +1,11 @@
 package com.ezen.springplanetrip.controller;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Map;
-
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,18 +21,28 @@ public class PassengerController {
 	
 	@PostMapping("/insertPassenger.do")
 	public String insertPassenger(@RequestParam Map<String, String> paramMap) {
-		
+		SimpleDateFormat smf = new SimpleDateFormat("HH:mm");
+		System.out.println("승객 작업 중..");
+
+
 		System.out.println(paramMap.toString());
 		
+		String departTime = smf.format(Date.valueOf(paramMap.get("departTime")));
+		//Date departTime = Date.valueOf(paramMap.get("departTime"));
+		System.out.println("출발 시간 정리 : " + departTime);
+		
+		String arrivalTime = smf.format(Date.valueOf(paramMap.get("arrivalTime")));
+		//Date arrivalTime = Date.valueOf(paramMap.get("arrivalTime"));
+		System.out.println("도착 시간 정리 : " + arrivalTime);
 //		List<PassengerVO> passengerList = new ArrayList<PassengerVO>();
 		
 		for(int i = 1; i <= 3; i++) {
 			PassengerVO passengerVO = new PassengerVO();
-			passengerVO.setGender(paramMap.get("gender" + i));
-			passengerVO.setFirstName(paramMap.get("fisrtName" + i));
-			passengerVO.setLastName(paramMap.get("lastName" + i));
-			passengerVO.setPhone(paramMap.get("phone" + i));
-			passengerVO.setBirthDate(Date.valueOf(paramMap.get("birthDate" + i)));
+			passengerVO.setGender((String)paramMap.get("gender" + i));
+			passengerVO.setFirstName((String)paramMap.get("fisrtName" + i));
+			passengerVO.setLastName((String)paramMap.get("lastName" + i));
+			passengerVO.setPhone((String)paramMap.get("phone" + i));
+			passengerVO.setBirthDate(Date.valueOf((String)paramMap.get("birthDate" + i)));
 			
 			passengerService.insertPassenger(passengerVO);
 		}
